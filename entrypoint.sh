990c5399347ff85abd976a5b8e14ff40241ef17a
firmanhrosid <firman.rosid@gmail.com>
2024-06-27 20:30:50 +0700

    updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,12 +30,13 @@ echo "Keep reports count ${INPUT_KEEP_REPORTS}"
 INPUT_KEEP_REPORTS=$((INPUT_KEEP_REPORTS + 1))
 echo "If ${COUNT} > ${INPUT_KEEP_REPORTS}"
 if ((COUNT > INPUT_KEEP_REPORTS)); then
+  cd ./${INPUT_ALLURE_HISTORY}
   echo "Echo 1"
   ls -d ${INPUT_REPORT_HISTORY}/*/ | sort -n
   echo "Echo 2"
   ls -d ${INPUT_REPORT_HISTORY}/*/ | sort -V
   echo "Removing the folder with the smallest number..."
-  ls | sort -V | grep -v 'CNAME' | head -n -$((${INPUT_KEEP_REPORTS} - 2)) | xargs rm -rv
+  ls | sort -V | head -n -$((${INPUT_KEEP_REPORTS} - 2)) | xargs rm -rv
   cd ${GITHUB_WORKSPACE}
 fi
 
